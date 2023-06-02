15b5f8c67bdef856340ef5ce8624ce67acdece15
dongjin6539 <dongjin6539@gmail.com>
2023-06-02 15:55:51 +0900

    2023.06.02

--- main/main.php
+++ main/main.php
@@ -16,7 +16,30 @@
     <title>PHP 블로그 만들기</title>
 
     <?php include "../include/head.php" ?>
-
+    <style>
+        .board__wrap {
+            width: 100%;
+            padding: 50px 0;
+        }
+        .board__wrap h2 {
+            display: inline-block;
+            border: 1px solid var(--color__black);
+            border-radius: 5px;
+            font-size: 0.8em;
+            padding: 0.1em 1em;
+            margin-bottom: 1.6em;
+        }
+        .board__wrap h2:hover {
+            background-color: var(--color__black);
+            color: var(--color__white);
+            cursor: grab;
+        } 
+        .board__wrap h2:hover a {
+            background-color: var(--color__black);
+            color: var(--color__white);
+            cursor: grab;
+        }
+    </style>
 </head>
 <body class="gray">
     
@@ -40,9 +63,30 @@
 
         <div class="blog__inner">
             <div class="blog__wrap bmStyle">
-                <h2>Javascript Topic</h2>
+                <h2><a href="../blog/blog.php">Blog</a></h2>
                 <div class="cards__inner">
-                    <div class="card">
+<?php
+    $sql = "SELECT * FROM blog WHERE blogDelete = 0 ORDER BY blogID DESC LIMIT 12";
+    $result = $connect -> query($sql);
+?>
+
+<?php foreach($result as $blog){ ?>
+    <div class="card">
+        <figure class="card__img">
+                <a href="../blog/blogView.php?blogID=<?= $blog['blogID']?>&category=<?= $blog['blogCategory']?>">
+                <img src="../assets/blog/<?= $blog['blogImgFile'] ?>" alt="<?= $blog['blogTitle'] ?>">
+            </a>
+        </figure>
+        <div class="card__title">
+            <h3><?= $blog['blogTitle'] ?></h3>
+            <p><?= htmlspecialchars_decode($blog['blogContents']) ?></p>
+        </div>                        
+        <div class="card__info">
+            <a href="#" class="more">더보기</a href="#">
+        </div>
+    </div>
+<?php } ?>
+                    <!-- <div class="card">
                         <figure class="card__img">
                             <source srcset="../assets/img/blog01.jpg, ../assets/img/blog01@2x.jpg 2x, ../assets/img/blog01@3x.jpg 3x" />
                             <img src="../assets/img/blog01.jpg" alt="소개이미지">
@@ -54,8 +98,8 @@
                         <div class="card__info">
                             <a href="#" class="more">더보기</a href="#">
                         </div>
-                    </div>
-                    <div class="card">
+                    </div> -->
+                    <!-- <div class="card">
                         <figure class="card__img">
                             <source srcset="../assets/img/blog02.jpg, ../assets/img/blog02@2x.jpg 2x, ../assets/img/blog02@3x.jpg 3x" />
                             <img src="../assets/img/blog02.jpg" alt="소개이미지">
@@ -93,13 +137,34 @@
                         <div class="card__info">
                             <a href="#" class="more">더보기</a href="#">
                         </div>
-                    </div>
+                    </div> -->
                 </div>
             </div>
             <div class="blog__wrap bmStyle">
-                <h2>Javascript Topic</h2>
+                <h2><a href="">Popular Blog</a></h2>
                 <div class="cards__inner col2 line2">
-                    <div class="card">
+<?php
+    $sql = "SELECT * FROM blog WHERE blogDelete = 0 ORDER BY blogView DESC LIMIT 4";
+    $result = $connect -> query($sql);
+?>
+
+<?php foreach($result as $blog){ ?>
+    <div class="card">
+        <figure class="card__img">
+                <a href="../blog/blogView.php?blogID=<?= $blog['blogID']?>&category=<?= $blog['blogCategory']?>">
+                <img src="../assets/blog/<?= $blog['blogImgFile'] ?>" alt="<?= $blog['blogTitle'] ?>">
+            </a>
+        </figure>
+        <div class="card__title">
+            <h3><?= $blog['blogTitle'] ?></h3>
+            <p><?= htmlspecialchars_decode($blog['blogContents']) ?></p>
+        </div>                        
+        <div class="card__info">
+            <a href="#" class="more">더보기</a href="#">
+        </div>
+    </div>
+<?php } ?>
+                    <!-- <div class="card">
                         <figure class="card__img">
                             <source srcset="../assets/img/blog01.jpg, ../assets/img/blog01@2x.jpg 2x, ../assets/img/blog01@3x.jpg 3x" />
                             <img src="../assets/img/blog01.jpg" alt="소개이미지">
@@ -112,139 +177,83 @@
                             <span class="author">dongjin</span>
                             <span class="date">2023.05.11</span>
                         </div>
-                    </div>
-                    <div class="card">
-                        <figure class="card__img">
-                            <source srcset="../assets/img/blog02.jpg, ../assets/img/blog02@2x.jpg 2x, ../assets/img/blog02@3x.jpg 3x" />
-                            <img src="../assets/img/blog02.jpg" alt="소개이미지">
-                        </figure>
-                        <div class="card__title">
-                            <h3>프로그래밍 언어 선택</h3>
-                            <p>코딩을 배우는 가장 일반적인 방법은 프로그래밍 언어를 배우는 것입니다. 많은 프로그래밍 언어 중에서는 Python, Java, C++, JavaScript 등이 널리 사용됩니다. 이 중에서 자신이 배우고 싶은 언어를 선택하고 해당 언어의 문법과 기초 지식을 습득하는 것이 좋습니다.</p>
-                        </div>                        
-                        <div class="card__info">
-                            <span class="author">dongjin</span>
-                            <span class="date">2023.05.11</span>
-                        </div>
-                    </div>
-                    <div class="card">
-                        <figure class="card__img">
-                            <source srcset="../assets/img/blog03.jpg, ../assets/img/blog03@2x.jpg 2x, ../assets/img/blog03@3x.jpg 3x" />
-                            <img src="../assets/img/blog03.jpg" alt="소개이미지">
-                        </figure>
-                        <div class="card__title">
-                            <h3>온라인 자료 활용</h3>
-                            <p>온라인에는 다양한 코딩 학습 자료가 있습니다. 강의, 튜토리얼, 문제 풀이 사이트 등을 활용하여 코딩을 배울 수 있습니다. 각 언어별로 제공되는 공식 문서와 레퍼런스를 참고하여 배우는 것도 좋은 방법입니다.</p>
-                        </div>                        
-                        <div class="card__info">
-                            <span class="author">dongjin</span>
-                            <span class="date">2023.05.11</span>
-                        </div>
-                    </div>
-                    <div class="card">
-                        <figure class="card__img">
-                            <source srcset="../assets/img/blog04.jpg, ../assets/img/blog04@2x.jpg 2x, ../assets/img/blog04@3x.jpg 3x" />
-                            <img src="../assets/img/blog04.jpg" alt="소개이미지">
-                        </figure>
-                        <div class="card__title">
-                            <h3>코딩 연습</h3>
-                            <p>코딩을 배운 후에는 많은 연습이 필요합니다. 간단한 예제부터 시작하여 점점 어려운 문제를 해결해 나가는 것이 좋습니다. 이를 통해 코딩 스킬을 향상시킬 수 있습니다.</p>
-                        </div>                        
-                        <div class="card__info">
-                            <span class="author">dongjin</span>
-                            <span class="date">2023.05.11</span>
-                        </div>
+                    </div> -->
+                </div>
+            </div>
+            
+                <div class="board__wrap">
+                    <h2><a href="../board/board.php">Board</a></h2>
+                    <div class="board__inner">
+                        <div class="board__table">
+                            <table>
+                                <colgroup>
+                                    <col style="width: 5%">
+                                    <col>
+                                    <col style="width: 10%">
+                                    <col style="width: 15%">
+                                    <col style="width: 7%">
+                                </colgroup>
+                                <thead>
+                                    <tr>
+                                        <th>번호</th>
+                                        <th>제목</th>
+                                        <th>등록자</th>
+                                        <th>등록일</th>
+                                        <th>조회수</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                <!-- <tr>
+                                    <td>1</td>
+                                    <td><a href="boardView.html">게시판 제목</a></td>
+                                    <td>신동진</td>
+                                    <td>2023-04-24</td>
+                                    <td>100</td>
+                                </tr> -->
+<?php
+    if(isset($_GET['page'])){
+        $page = (int) $_GET['page'];
+    } else {
+        $page = 1;
+    }
+
+    $viewNum = 10;
+    $viewLimit = ($viewNum * $page) - $viewNum;
+
+    // 1~20  DESC LIMIT 0,  20 --> page1 (viewNum * 1) - viewNum
+    // 21~40 DESC LIMIT 20, 20 --> page2 (viewNum * 2) - viewNum
+    // 41~60 DESC LIMIT 40, 20 --> page3 (viewNum * 3) - viewNum
+    // 61~80 DESC LIMIT 60, 20 --> page4 (viewNum * 4) - viewNum
+
+    $sql = "SELECT b.boardID, b.boardTitle, m.youName, b.regTime, b.boardView FROM board b JOIN members m ON (b.memberID = m.memberID) ORDER BY boardID DESC LIMIT {$viewLimit}, {$viewNum}";
+    $result = $connect -> query($sql);
+
+    if($result){
+        $count = $result -> num_rows;
+
+        if($count > 0){
+            for($i=0; $i<$count; $i++){
+                $info = $result -> fetch_array(MYSQLI_ASSOC);
+
+                echo "<tr>";
+                echo "<td>".$info['boardID']."</td>";
+                echo "<td><a href='../board/boardView.php?boardID={$info['boardID']}'>".$info['boardTitle']."</a></td>";
+                echo "<td>".$info['youName']."</td>";
+                echo "<td>".date('Y-m-d', $info['regTime'])."</td>";
+                echo "<td>".$info['boardView']."</td>";
+                echo "</tr>";
+            }
+        }  else {
+            echo "<tr><td colspan='5'>게시글이 없습니다.</td></tr>";
+        }
+    }
+?>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
-            <div class="blog__wrap bmStyle">
-                <h2>Javascript Topic</h2>
-                <div class="cards__inner col3 line3">
-                    <div class="card">
-                        <figure class="card__img">
-                            <source srcset="../assets/img/blog01.jpg, ../assets/img/blog01@2x.jpg 2x, ../assets/img/blog01@3x.jpg 3x" />
-                            <img src="../assets/img/blog01.jpg" alt="소개이미지">
-                        </figure>
-                        <div class="card__title">
-                            <h3>컴퓨터 과학 기초 지식 습득</h3>
-                            <p>프로그래밍 언어를 배우기 전에 컴퓨터 과학 기초 지식을 습득하는 것이 중요합니다. 이에는 컴퓨터의 작동 원리, 알고리즘, 데이터 구조 등이 포함됩니다. 이러한 기초적인 지식을 습득하면 프로그래밍 언어를 배우는데 큰 도움이 됩니다.</p>
-                        </div>                        
-                        <div class="card__info">
-                            <span class="author">dongjin</span>
-                            <span class="date">2023.05.11</span>
-                        </div>
-                    </div>
-                    <div class="card">
-                        <figure class="card__img">
-                            <source srcset="../assets/img/blog02.jpg, ../assets/img/blog02@2x.jpg 2x, ../assets/img/blog02@3x.jpg 3x" />
-                            <img src="../assets/img/blog02.jpg" alt="소개이미지">
-                        </figure>
-                        <div class="card__title">
-                            <h3>프로그래밍 언어 선택</h3>
-                            <p>코딩을 배우는 가장 일반적인 방법은 프로그래밍 언어를 배우는 것입니다. 많은 프로그래밍 언어 중에서는 Python, Java, C++, JavaScript 등이 널리 사용됩니다. 이 중에서 자신이 배우고 싶은 언어를 선택하고 해당 언어의 문법과 기초 지식을 습득하는 것이 좋습니다.</p>
-                        </div>                        
-                        <div class="card__info">
-                            <span class="author">dongjin</span>
-                            <span class="date">2023.05.11</span>
-                        </div>
-                    </div>
-                    <div class="card">
-                        <figure class="card__img">
-                            <source srcset="../assets/img/blog03.jpg, ../assets/img/blog03@2x.jpg 2x, ../assets/img/blog03@3x.jpg 3x" />
-                            <img src="../assets/img/blog03.jpg" alt="소개이미지">
-                        </figure>
-                        <div class="card__title">
-                            <h3>온라인 자료 활용</h3>
-                            <p>온라인에는 다양한 코딩 학습 자료가 있습니다. 강의, 튜토리얼, 문제 풀이 사이트 등을 활용하여 코딩을 배울 수 있습니다. 각 언어별로 제공되는 공식 문서와 레퍼런스를 참고하여 배우는 것도 좋은 방법입니다.</p>
-                        </div>                        
-                        <div class="card__info">
-                            <span class="author">dongjin</span>
-                            <span class="date">2023.05.11</span>
-                        </div>
-                    </div>
-                    <div class="card">
-                        <figure class="card__img">
-                            <source srcset="../assets/img/blog04.jpg, ../assets/img/blog04@2x.jpg 2x, ../assets/img/blog04@3x.jpg 3x" />
-                            <img src="../assets/img/blog04.jpg" alt="소개이미지">
-                        </figure>
-                        <div class="card__title">
-                            <h3>코딩 연습</h3>
-                            <p>코딩을 배운 후에는 많은 연습이 필요합니다. 간단한 예제부터 시작하여 점점 어려운 문제를 해결해 나가는 것이 좋습니다. 이를 통해 코딩 스킬을 향상시킬 수 있습니다.</p>
-                        </div>                        
-                        <div class="card__info">
-                            <span class="author">dongjin</span>
-                            <span class="date">2023.05.11</span>
-                        </div>
-                    </div>
-                    <div class="card">
-                        <figure class="card__img">
-                            <source srcset="../assets/img/blog05.jpg, ../assets/img/blog05@2x.jpg 2x, ../assets/img/blog05@3x.jpg 3x" />
-                            <img src="../assets/img/blog05.jpg" alt="소개이미지">
-                        </figure>
-                        <div class="card__title">
-                            <h3>프로젝트 수행</h3>
-                            <p>프로그래밍 언어를 배운 후, 개인 프로젝트를 수행하는 것이 좋습니다. 이를 통해 배운 지식을 실제로 적용하고, 문제를 해결하는 경험을 쌓을 수 있습니다.</p>
-                        </div>                        
-                        <div class="card__info">
-                            <a href="#" class="more">더보기</a href="#">
-                        </div>
-                    </div>
-                    <div class="card">
-                        <figure class="card__img">
-                            <source srcset="../assets/img/blog06.jpg, ../assets/img/blog06@2x.jpg 2x, ../assets/img/blog06@3x.jpg 3x" />
-                            <img src="../assets/img/blog06.jpg" alt="소개이미지">
-                        </figure>
-                        <div class="card__title">
-                            <h3>지속적인 학습</h3>
-                            <p>코딩을 배우는 것은 끊임없는 학습과 발전이 필요합니다. 새로운 기술이나 프레임워크가 나타날 때마다 학습하고 적용해 나가는 것이 좋습니다.</p>
-                        </div>                        
-                        <div class="card__info">
-                            <a href="#" class="more">더보기</a href="#">
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="blog__wrap">
+            <!-- <div class="blog__wrap">
                 <h2>Javascript Topic</h2>
                 <div class="cards__inner col6 line2">
                     <div class="card">
@@ -326,7 +335,7 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> -->
         </div>
         <!-- 
             <div class="intro__inner"></div> 각 페이지 소개 배너
